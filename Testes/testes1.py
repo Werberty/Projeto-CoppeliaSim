baa9973edd9ce13ef335f6a42f8e15c25b5873bb
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-04-01 17:17:41 -0300

    Create testes1.py

---
+++ Testes/testes1.py
@@ -0,0 +1,95 @@
+try:
+    import sim
+except:
+    print ('--------------------------------------------------------------')
+    print ('"sim.py" could not be imported. This means very probably that')
+    print ('either "sim.py" or the remoteApi library could not be found.')
+    print ('Make sure both are in the same folder as this file,')
+    print ('or appropriately adjust the file "sim.py"')
+    print ('--------------------------------------------------------------')
+    print ('')
+
+import time
+import numpy as np
+
+print ('Program started')
+sim.simxFinish(-1) # just in case, close all opened connections
+clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
+
+if clientID!=-1:
+    print ('Connected to remote API server')
+
+    # Iniciando a simulação
+    # Deve usar a porta do 'continuous remote API server services' (remoteApiConnections.txt)
+    # e = sim.simxStartSimulation(clientID,sim.simx_opmode_blocking)
+
+    # Handle para o ROBÔ    
+    robotname = 'PioneerP3DX'
+    returnCode, robotHandle = sim.simxGetObjectHandle(clientID, robotname, sim.simx_opmode_oneshot_wait)     
+    
+    # Handle para as juntas das RODAS
+    returnCode, l_wheel = sim.simxGetObjectHandle(clientID, robotname + '_leftMotor', sim.simx_opmode_oneshot_wait)
+    returnCode, r_wheel = sim.simxGetObjectHandle(clientID, robotname + '_rightMotor', sim.simx_opmode_oneshot_wait)    
+    
+    # # Handle para os dados do LASER
+    # laser_range_data = "hokuyo_range_data"
+    # laser_angle_data = "hokuyo_angle_data"
+    
+    # # Geralmente a primeira leitura é inválida (atenção ao Operation Mode)
+    # # Em loop até garantir que as leituras serão válidas
+    # returnCode = 1
+    # while returnCode != 0:
+    #     returnCode, range_data = sim.simxGetStringSignal(clientID, laser_range_data, sim.simx_opmode_streaming + 10)
+    
+    # # Prosseguindo com as leituras
+    # raw_range_data, raw_angle_data = readSensorData(clientID, laser_range_data, laser_angle_data)
+    # laser_data = np.array([raw_angle_data, raw_range_data]).T
+    
+    returnCode, pos = sim.simxGetObjectPosition(clientID, robotHandle, -1, sim.simx_opmode_oneshot_wait)        
+    print('Pos: ', pos)
+    
+    #raise SystemExit()
+    
+    # Dados do Pioneer
+    L = 0.381   # Metros
+    r = 0.0975  # Metros
+
+    t = 0
+    # Lembrar de habilitar o 'Real-time mode'
+    startTime=time.time()
+    lastTime = startTime
+    while t < 10:
+        
+        now = time.time()
+        dt = now - lastTime
+
+        # Velocidade básica (linear, angular)
+        v = .5
+        w = 0
+        # w = np.deg2rad(0)
+
+        # Isso é o modelo cinemático, estudaremos detalhadamente depois!
+        wl = v/r - (w*L)/(2*r)
+        wr = v/r + (w*L)/(2*r)
+        
+        # Enviando velocidades
+        sim.simxSetJointTargetVelocity(clientID, l_wheel, wl, sim.simx_opmode_streaming + 5)
+        sim.simxSetJointTargetVelocity(clientID, r_wheel, wr, sim.simx_opmode_streaming + 5)
+
+        t = t + dt  
+        lastTime = now
+    
+    # Parando o robô    
+    sim.simxSetJointTargetVelocity(clientID, r_wheel, 0, sim.simx_opmode_oneshot_wait)
+    sim.simxSetJointTargetVelocity(clientID, l_wheel, 0, sim.simx_opmode_oneshot_wait)        
+       
+    # Parando a simulação     
+    sim.simxStopSimulation(clientID,sim.simx_opmode_blocking)         
+        
+    # Now close the connection to CoppeliaSim:
+    sim.simxFinish(clientID)
+
+else:
+    print ('Failed connecting to remote API server')
+    
+print ('Program ended')
