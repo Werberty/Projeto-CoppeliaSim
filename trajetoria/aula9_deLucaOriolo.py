94b14227faca4eb1b6ea7cabfd382c3978a6d958
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-04-24 15:43:45 -0300

    bug algoritimo

---
+++ trajetoria/aula9_deLucaOriolo.py
@@ -0,0 +1,114 @@
+try:
+    import sim
+except:
+    print('--------------------------------------------------------------')
+    print('"sim.py" could not be imported. This means very probably that')
+    print('either "sim.py" or the remoteApi library could not be found.')
+    print('Make sure both are in the same folder as this file,')
+    print('or appropriately adjust the file "sim.py"')
+    print('--------------------------------------------------------------')
+    print('')
+
+
+import time
+
+import numpy as np
+
+
+def Rz(theta):
+
+    return np.array([[np.cos(theta), -np.sin(theta), 0],
+                     [np.sin(theta), np.cos(theta), 0],
+                     [0, 0, 1]])
+
+# Normalize angle to the range [-pi,pi)
+
+
+print('Program started')
+sim.simxFinish(-1)  # just in case, close all opened connections
+clientID = sim.simxStart('127.0.0.1', 19999, True, True,
+                         5000, 5)  # Connect to CoppeliaSim
+
+if clientID != -1:
+    print('Connected to remote API server')
+
+    robotname = 'Pioneer_p3dx'
+    returnCode, robotHandle = sim.simxGetObjectHandle(
+        clientID, robotname, sim.simx_opmode_oneshot_wait)
+
+    returnCode, robotLeftMotorHandle = sim.simxGetObjectHandle(
+        clientID, robotname + '_leftMotor', sim.simx_opmode_oneshot_wait)
+    returnCode, robotRightMotorHandle = sim.simxGetObjectHandle(
+        clientID, robotname + '_rightMotor', sim.simx_opmode_oneshot_wait)
+
+    returnCode, robotPos = sim.simxGetObjectPosition(
+        clientID, robotHandle, -1, sim.simx_opmode_oneshot_wait)
+    returnCode, robotOri = sim.simxGetObjectOrientation(
+        clientID, robotHandle, -1, sim.simx_opmode_oneshot_wait)
+
+    # Goal position (x, y)
+    list_pgoal = [np.array([4, 0]), np.array([4, -4]), np.array([0, 0])]
+    # pgoal = np.array([4, 0])
+    # pgoal = np.array([0, 0])
+
+    for pgoal in list_pgoal:
+        # Frame que representa o Goal
+        returnCode, goalFrame = sim.simxGetObjectHandle(
+            clientID, 'Goal', sim.simx_opmode_oneshot_wait)
+        returnCode = sim.simxSetObjectPosition(
+            clientID, goalFrame, -1, [pgoal[0], pgoal[1], 0], sim.simx_opmode_oneshot_wait)
+        returnCode = sim.simxSetObjectOrientation(
+            clientID, goalFrame, -1, [0, 0, 0], sim.simx_opmode_oneshot_wait)
+
+        # Específico do robô
+        # https://www.generationrobots.com/media/Pioneer3DX-P3DX-RevA.pdf
+        L = 0.381
+        r = 0.0975
+        maxv = 1.0
+        maxw = np.deg2rad(45)
+
+        rho = np.inf
+    
+        while rho > .05:
+
+            returnCode, robotPos = sim.simxGetObjectPosition(
+                clientID, robotHandle, -1, sim.simx_opmode_oneshot_wait)
+            returnCode, robotOri = sim.simxGetObjectOrientation(
+                clientID, robotHandle, -1, sim.simx_opmode_oneshot_wait)
+            robotConfig = np.array([robotPos[0], robotPos[1], robotOri[2]])
+
+            dx, dy = pgoal - robotConfig[:2]
+
+            # Apenas para interromper o loop
+            rho = np.sqrt(dx**2 + dy**2)
+
+            kr = 1 
+            kt = 2 
+
+            v = kr*(dx*np.cos(robotConfig[2]) + dy*np.sin(robotConfig[2]))
+            w = kt*(np.arctan2(dy, dx) - robotConfig[2])
+
+            # Limit v,w to +/- max
+            v = max(min(v, maxv), -maxv)
+            w = max(min(w, maxw), -maxw)
+
+            vr = ((2.0*v) + (w*L))/(2.0*r)
+            vl = ((2.0*v) - (w*L))/(2.0*r)
+
+            sim.simxSetJointTargetVelocity(
+                clientID, robotRightMotorHandle, vr, sim.simx_opmode_oneshot_wait)
+            sim.simxSetJointTargetVelocity(
+                clientID, robotLeftMotorHandle, vl, sim.simx_opmode_oneshot_wait)
+
+    sim.simxSetJointTargetVelocity(
+        clientID, robotRightMotorHandle, 0, sim.simx_opmode_oneshot_wait)
+    sim.simxSetJointTargetVelocity(
+        clientID, robotLeftMotorHandle, 0, sim.simx_opmode_oneshot_wait)
+
+    # Now close the connection to CoppeliaSim:
+    sim.simxFinish(clientID)
+
+else:
+    print('Failed connecting to remote API server')
+
+print('Program ended')
